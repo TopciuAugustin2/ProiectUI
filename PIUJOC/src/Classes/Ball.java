6d6b1b14e74035073581a3be06676a9e9a4c8c6f
Stefania164 <stefania.luca@student.tuiasi.ro>
2024-01-10 12:03:38 +0200

    Update Ball.java

--- PIUJOC/src/Classes/Ball.java
+++ PIUJOC/src/Classes/Ball.java
@@ -1,10 +1,10 @@
 package Classes;
 
 import Parents.Entity;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Ball extends Entity {
 
@@ -16,35 +16,32 @@ public class Ball extends Entity {
     public double speedY;
     Rectangle collider;
 
+    private String currentDirection = "right";
+
 
     //constructor -- initializare prop minge/mingi
-    public Ball(GamePanel GP,Player player)
-    {
-        this.p=player;
-        this.gp=GP;
-        this.speed=3 * 2;
+    public Ball(GamePanel GP, Player player) {
+        this.p = player;
+        this.gp = GP;
+        this.speed = 3 * 2;
         this.speedX = speed;
         this.speedY = speed;
-        this.direction = new Vector2D(300,200);
-        solidArea = new Rectangle (300,200,25,25);
+        this.direction = new Vector2D(300, 200);
+        solidArea = new Rectangle(300, 200, 25, 25);
         getBallImage();
-        this.dmg=1;
-        this.collider = new Rectangle(0,p.y-20,gp.screenSize.width,1);
+        this.dmg = 1;
+        this.collider = new Rectangle(0, p.y - 20, gp.screenSize.width, 1);
 
     }
 
 
-
-    public void getBallImage()
-    {
-        try{
+    public void getBallImage() {
+        try {
             skin = ImageIO.read(getClass().getResourceAsStream("/Classes/ball1.png"));
-        }catch(IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 
 
     public void setDirection(Vector2D newDirection) {
@@ -56,38 +53,19 @@ public class Ball extends Entity {
         return new Rectangle(x, y, diameter, diameter);
     }
 
-    public void update()
-    {
+    public void update() {
         direction.x += speedX;
         direction.y += speedY;
 
         solidArea.x += speedX;
         solidArea.y += speedY;
 
-        double playerIntersectsColliderX = solidArea.intersection(collider).getX();
-        double playerIntersectsColliderY = solidArea.intersection(collider).getY();
-
-        if(solidArea.intersects(p.solidArea)) {
+        if (solidArea.intersects(p.solidArea)) {
             // punctul de coliziune al bilei cu playerul
-
-            double hitPointX = solidArea.intersection(p.solidArea).getX();
-            double hitPointY = solidArea.intersection(p.solidArea).getY();
-
             double paddleCenterX = p.solidArea.getCenterX();
 
             //diferenta de centru player si hit point
 
-            double dif = paddleCenterX - hitPointX;
-
-            Vector2D reflectionVector;
-
-            Vector2D cornerLeft = new Vector2D(p.solidArea.getX(), p.solidArea.getY());
-            Vector2D cornerRight = new Vector2D(p.solidArea.getX() + p.solidArea.width, p.solidArea.getY());
-
-            Vector2D intersection = new Vector2D(playerIntersectsColliderX, playerIntersectsColliderY);
-
-            double distanceLeft = direction.distance(cornerLeft);
-            double distanceRight = direction.distance(cornerRight);
 
             if (solidArea.x < paddleCenterX) {
                 speedY = -speedY;
@@ -95,52 +73,65 @@ public class Ball extends Entity {
                 speedY = -speedY;
             }
 
-            solidArea.x=(int)direction.x;
-            solidArea.y=(int)direction.y;
+            Rectangle lefSideOfPaddle = new Rectangle((int) p.solidArea.getX()
+                    , (int) p.solidArea.getY(),
+                    (int) (p.solidArea.getWidth() / 2 - p.solidArea.height),
+                    (int) p.solidArea.getHeight());
+
+            Rectangle rightSideOfPaddle = new Rectangle((int) (p.solidArea.getX() + (p.solidArea.getWidth() / 2 + p.solidArea.height))
+                    , (int) p.solidArea.getY(),
+                    (int) (p.solidArea.getWidth() / 2 - p.solidArea.height),
+                    (int) p.solidArea.getHeight());
+            if (currentDirection.equals("right") && solidArea.intersects(lefSideOfPaddle)) {
+                reverseXDirection();
+            } else if (currentDirection.equals("left") && solidArea.intersects(rightSideOfPaddle)) {
+                reverseXDirection();
+            }
+            solidArea.x = (int) direction.x;
+            solidArea.y = (int) direction.y;
         }
 
         if (gp.leftWall.solidArea.intersects(solidArea)) {
-            //reverseXDirection();
-            speedX = -speedX;
-            solidArea.x=(int)direction.x;
-            solidArea.y=(int)direction.y;
+            reverseXDirection();
         }
         if (gp.rightWall.solidArea.intersects(solidArea)) {
-            //reverseYDirection();
-            speedX = -speedX;
-            solidArea.x=(int)direction.x;
-            solidArea.y=(int)direction.y;
+            reverseXDirection();
         }
         if (gp.topWall.solidArea.intersects(solidArea)) {
-            //reverseYDirection();
-            speedY = -speedY;
-            solidArea.x=(int)direction.x;
-            solidArea.y=(int)direction.y;
+            reverseYDirection();
         }
 
-        if(solidArea.y >= gp.screenHeight+10)
-        {
-            gp.gameState=gp.gameOverState;
+        if (solidArea.y >= gp.screenHeight + 10) {
+            gp.gameState = gp.gameOverState;
 
         }
-
-
     }
 
     public void reverseYDirection() {
-        direction = direction.reflectX();
-        speedX = -speedX;
-    }
-    public void reverseXDirection() {
-        direction = direction.reflectY();
         speedY = -speedY;
+        solidArea.x = (int) direction.x;
+        solidArea.y = (int) direction.y;
     }
 
-    public void draw(Graphics2D g2)
-    {
+    public void reverseXDirection() {
+        speedX = -speedX;
+        solidArea.x = (int) direction.x;
+        solidArea.y = (int) direction.y;
+        reverseCurrentDirection();
+    }
+
+    public void draw(Graphics2D g2) {
         g2.draw(solidArea);
-        g2.draw(new Rectangle(0,0,30*16,20*16));
-        g2.drawImage(skin,(int)direction.getX(),(int)direction.getY(),25,25,null);
+        g2.draw(new Rectangle(0, 0, 30 * 16, 20 * 16));
+        g2.drawImage(skin, (int) direction.getX(), (int) direction.getY(), 25, 25, null);
         g2.draw(collider);
+    }
+
+    private void reverseCurrentDirection() {
+        if (currentDirection.equals("right")) {
+            currentDirection = "left";
+        } else {
+            currentDirection = "right";
+        }
     }
 }
